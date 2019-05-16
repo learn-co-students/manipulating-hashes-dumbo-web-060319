5fcbc34aa1df8b817912223029df49e6379b192b
Alexander Schelchere <ascellie@gmail.com>
2019-05-16 10:32:12 -0400

    Fred no like strawberry

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -15,6 +15,13 @@ def first_challenge
   }
 
   #your code here
+  fred_no_like = contacts["Freddy Mercury"][:favorite_icecream_flavors]
+
+  fred_no_like.each do |flavor|
+    if flavor.downcase == "strawberry"
+      fred_no_like.delete("strawberry")
+    end
+  end
 
 
   #remember to return your newly altered contacts hash!
